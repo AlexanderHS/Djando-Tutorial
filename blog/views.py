81c18090beb2a2d258af00bbb4c9f88629264f0a
alexhamiltonsmith@gmail.com <u6087050@anu.edu.au>
2019-06-09 13:25:29 +1000

    Connected Post object to view and template.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 '''
 This is a view.
 '''
@@ -8,4 +10,5 @@ def post_list(request):
     """
     sdfgsdfg
     """
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
